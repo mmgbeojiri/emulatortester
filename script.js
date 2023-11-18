0190b5c994192ad05924f5aca74879b2ca744486
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2023-11-17 20:44:23 -0500

    added test features in script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Get the current URL of the website
+var url = window.location.href;
+
+// Create a new URLSearchParams object with the query string
+var params = new URLSearchParams(url.split('?')[1]);
+
+// Get the value of the name parameter
+var name = params.get('name');
+
+// Get the value of the age parameter
+var age = params.get('age');
+
+// Display the values in the console
+console.log('Name: ' + name);
+console.log('Age: ' + age);
